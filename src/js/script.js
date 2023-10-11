a3457c84cbc19c3b0aa32766cd6cfae3239aaa96
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-11 00:24:11 -0300

    Create script.js

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const valor = document.getElementById("valor")
+const maisButton = document.getElementById("mais")
+const menosButton = document.getElementById("menos")
+const resetarButton = document.getElementById("resetar")
+
+let contador = 0;
+
+maisButton.addEventListener('mousedown', () => {
+    
+})
+
