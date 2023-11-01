40da55870a6dd2775959d12692a2d0c6e0ee848a
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-31 23:16:46 -0300

    contadorJS

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,31 @@
-const valor = document.getElementById("valor")
-const maisButton = document.getElementById("mais")
-const menosButton = document.getElementById("menos")
-const resetarButton = document.getElementById("resetar")
+const valor = document.getElementById("valor");
+const maisButton = document.getElementById("mais");
+const menosButton = document.getElementById("menos");
+const resetarButton = document.getElementById("resetar");
 
 let contador = 0;
+let intervalId = 0;
 
-maisButton.addEventListener('mousedown', () => {
-    
-})
+const atualizarValor = () => {
+  valor.innerHTML = contador;
+};
+maisButton.addEventListener("mousedown", () => {
+  intervalId = setInterval(() => {
+    contador++;
+    atualizarValor();
+  }, 100);
+});
 
+menosButton.addEventListener("mousedown", () => {
+  intervalId = setInterval(() => {
+    contador--;
+    atualizarValor();
+  }, 100);
+});
+
+resetarButton.addEventListener("click", () => {
+  contador = 0;
+  atualizarValor();
+});
+
+document.addEventListener("mouseup", () => clearInterval(intervalId));
